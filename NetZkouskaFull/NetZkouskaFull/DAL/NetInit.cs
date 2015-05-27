01bd733510263a22bbfabda2a69fc3120b411f00
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-05-27 09:52:05 +0200

    Metody pro výpočty

---
+++ NetZkouskaFull/NetZkouskaFull/DAL/NetInit.cs
@@ -0,0 +1,45 @@
+﻿using NetZkouskaFull.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetZkouskaFull.DAL
+{
+	public class NetInit : System.Data.Entity.DropCreateDatabaseAlways<NetContext>
+	{
+		protected override void Seed(NetContext context)
+		{
+			Work w1 = new Work{Points = 45};
+
+			Student s1 = new Student{Name="Carson",Lastname="Alexander",StudentIdentity="a10b0618p"};
+
+			var students = new List<Student>
+			{
+			new Student{Name="Meredith",Lastname="Alonso",StudentIdentity="a10b0618p"},
+			new Student{Name="Arturo",Lastname="Anand",StudentIdentity="a10b0618p"},
+			new Student{Name="Gytis",Lastname="Barzdukas",StudentIdentity="a10b0618p"},
+			new Student{Name="Carson",Lastname="Alexander",StudentIdentity="a10b0618p"}
+			};
+
+			Random r = new Random(75);
+			foreach(Student s in students) {
+				List<Exam> Exams = new List<Exam>();
+				Exams.Add(new Exam { Points = r.Next(80), DownDate = DateTime.Parse("2015-01-02") });
+				s.Exams = Exams;
+
+				List<Work> Works = new List<Work>();
+				Works.Add(new Work { Points = r.Next(80), DownDate = DateTime.Parse("2015-01-02"), Comment="" });
+				s.Works = Works;
+			}
+
+			//new Student{Name="Yan",Lastname="Li",EnrollmentDate=DateTime.Parse("2002-09-01")},
+			//new Student{Name="Peggy",Lastname="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
+			//new Student{Name="Laura",Lastname="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
+			//new Student{Name="Nino",Lastname="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+
+			students.ForEach(s => context.Students.Add(s));
+			context.SaveChanges();
+		}
+	}
+}
--- NetZkouskaFull/NetZkouskaFull/Models/Student.cs
+++ NetZkouskaFull/NetZkouskaFull/Models/Student.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Xml.Linq;
 
 namespace NetZkouskaFull.Models
 {
@@ -18,5 +21,91 @@ namespace NetZkouskaFull.Models
 		public virtual ICollection<Exam> Exams { get; set; }
 
 		public virtual ICollection<Work> Works { get; set; }
+
+		[NotMapped]
+		public string HasWorkDown { 
+			get{
+				IEnumerable Config = GetConfig("work");
+
+				int workPoints = 0;
+				foreach(XElement g in Config) {
+					workPoints = (int) g.Descendants("workPoints").FirstOrDefault();
+				}
+
+				foreach(Work w in Works) {
+					if(w.Points >= workPoints) {
+						return "Ma";
+					}
+				}
+
+				return "Nema";
+			}
+		}
+
+		[NotMapped]
+		public int Grade
+		{
+			get
+			{
+				IEnumerable Config = GetConfig("exam");
+
+				int one = 0;
+				int two = 0;
+				int three = 0;
+				foreach (XElement g in Config)
+				{
+					one = (int)g.Descendants("one").FirstOrDefault();
+					two = (int)g.Descendants("two").FirstOrDefault();
+					three = (int)g.Descendants("three").FirstOrDefault();
+				}
+
+				int bestScore = 0;
+				foreach (Exam e in Exams)
+				{
+					bestScore = Math.Max(bestScore, e.Points);
+				}
+
+				if (bestScore >= one)
+				{
+					return 1;
+				}
+				if (bestScore >= two)
+				{
+					return 2;
+				}
+				if (bestScore >= three)
+				{
+					return 3;
+				}
+
+				return 4;
+			}
+		}
+
+		[NotMapped]
+		public int GradePoints
+		{
+			get
+			{
+				int bestScore = 0;
+				foreach (Exam e in Exams)
+				{
+					bestScore = Math.Max(bestScore, e.Points);
+				}
+
+				return bestScore;
+			}
+		}
+
+
+
+		private IEnumerable GetConfig(string ConfigName) {
+			XDocument configXML = XDocument.Load("D:\\škola\\netZkouskaFull\\config.xml");
+
+			IEnumerable configQuery = from c in configXML.Descendants("configs").Descendants(ConfigName)
+							 select c;// getting ship node in ships XML
+
+			return configQuery;
+		}
 	}
 }
